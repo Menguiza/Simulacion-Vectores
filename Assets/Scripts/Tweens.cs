0be1119ae43a908ae13287436320d589a5a132f2
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2022-10-30 22:54:24 -0500

    Tweens And Ease

---
+++ Assets/Scripts/Tweens.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweens : MonoBehaviour
+{
+    [SerializeField] float time;
+    [SerializeField, Range(0, 1)] float tParameter = 0;
+    [SerializeField] Color initialColor, targetColor;
+    [SerializeField] private AnimationCurve curve;
+    [SerializeField] Transform target;
+    
+    //Variables de Utilidad
+    float currentTime;
+    Vector3 initialPosition, targetPosition;
+    new Renderer renderer;
+    private bool easeActive, tweenInfo;
+    
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        if (tweenInfo)
+        {
+            tParameter = currentTime / time;
+        
+            if (!easeActive)
+            {
+                transform.position = Vector3.LerpUnclamped(initialPosition, targetPosition, curve.Evaluate(tParameter));
+                renderer.material.color = Color.LerpUnclamped(initialColor, targetColor, curve.Evaluate(tParameter));
+            }
+            else
+            {
+                transform.position = Vector3.LerpUnclamped(initialPosition, targetPosition, EaseInElastic(tParameter));
+                renderer.material.color = Color.LerpUnclamped(initialColor, targetColor, EaseInElastic(tParameter));
+            }
+        
+            currentTime += Time.deltaTime;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Tween();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ChangeEase();
+        }
+    }
+
+    private void Tween()
+    {
+        tParameter = 0;
+        currentTime = 0;
+        initialPosition = transform.position;
+        targetPosition = target.position;
+        tweenInfo = true;
+    }
+
+    private float EaseInElastic(float x)
+    {
+        float c5 = (2f * Mathf.PI) / 4.5f;
+        return x == 0f ? 0f : x == 1f ? 1f : x < 0.5 ? -(Mathf.Pow(2f, 20f * x - 10f) * Mathf.Sin((20f * x - 11.125f) * c5)) / 2f : (Mathf.Pow(2f, -20f * x + 10f) * Mathf.Sin((20f * x - 11.125f) * c5)) / 2f + 1f;
+    }
+
+    private void ChangeEase()
+    {
+        easeActive = !easeActive;
+        Debug.Log("Changed");
+    }
+}
